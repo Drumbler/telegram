112a0324bf7252ac1f43df0a08559ea92afc3804
Andrey <andy132012@gmail.com>
2023-04-23 17:23:45 +0500

    ТГ бот

---
+++ src/main/java/org/example/TestBot.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TestBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()){
+            Message message = update.getMessage();
+            if (message.hasText()){
+                try {
+                    execute(SendMessage.builder().chatId(message.getChatId().toString()).text("гыг").build());
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "@Rodionov_testbot";
+    }
+
+    @Override
+    public String getBotToken(){
+        return "5718084324:AAEblt3PXVm3sq-7v5dftQcKeXqX-TRQuyA";
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TestBot testBot = new TestBot();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(testBot);
+    }
+
+}
